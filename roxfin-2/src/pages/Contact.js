f39fbe38a4c7fca4686b243b177b64d580e8931f
smithgrx19 <50107233+smithgrx19@users.noreply.github.com>
2025-06-16 18:13:23 +0200

    Add files via upload

---
+++ roxfin-2/src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <section className="page">
+    <h1>Contact Us</h1>
+    <p>Email: info@roxfin.co.za</p>
+    <p>Phone: +27 12 345 6789</p>
+  </section>
+);
+
+export default Contact;
