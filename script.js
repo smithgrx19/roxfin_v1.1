5123951b83ec91dd99b2f7d1d7c61443aed4cd73
smithgrx19 <50107233+smithgrx19@users.noreply.github.com>
2025-06-23 19:57:15 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("confirmation").style.display = "block";
-  this.reset();
-});
