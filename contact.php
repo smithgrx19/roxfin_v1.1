513c418263227899833d964454915821112dd689
smithgrx19 <50107233+smithgrx19@users.noreply.github.com>
2025-06-30 14:26:27 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "admin@roxfin.co.za"; // Change this to your email
+    $subject = "New Contact Form Submission";
+
+    $name = htmlspecialchars($_POST['name']);
+    $phone =htmlspecialchars($_POST['phone']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message failed to send.";
+    }
+}
+?>
